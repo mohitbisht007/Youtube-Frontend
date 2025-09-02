d9a90f95b011be8b0f37a83013336a1587546182
Mohit <mohitbisht5678@gmail.com>
2025-09-02 23:32:31 +0530

    changed baseURL

--- src/helpers/axiosInterceptor.js
+++ src/helpers/axiosInterceptor.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5050"
+  baseURL: "https://youtube-backend-bl51.onrender.com"
 });
 
 api.interceptors.request.use((config) => {
